e6ec4c35fd678393e427d6e4589144e02bceb7cb
doraemon-daffodils <81766259+doraemon-daffodils@users.noreply.github.com>
2022-05-04 19:52:26 +0530

    pwa

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,4 @@
 module.exports = {
   extends: '@antfu',
-  rules: {
-    'no-console': 'off',
-    'import/no-unresolved': 'off',
-    'import/extensions': 'off',
-    'import/no-extraneous-dependencies': 'off',
-    'vue/component-tags-order': 'off',
-  },
+  
 }
