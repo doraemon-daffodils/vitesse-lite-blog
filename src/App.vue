6347c7c628da52f08e16bb42aba527aa72b7a88c
doraemon-daffodils <81766259+doraemon-daffodils@users.noreply.github.com>
2022-05-04 20:13:09 +0530

    lets try

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,15 @@
-<template>
-  <NavBar />
-  <main class="px-7 py-10">
-    <article>
-      <router-view />
-    </article>
-    <Footer />
-  </main>
-</template>
-
 <script setup lang="ts">
 // https://github.com/vueuse/head
 // you can use this to manipulate the document head in any components,
 // they will be rendered correctly in the html results with vite-ssg
 useHead({
-  title: 'HDS BLOG',
+  title: 'Vitesse',
   meta: [
-    { name: 'HDS BLOG', content: 'To keep track of things' },
+    { name: 'description', content: 'Opinionated Vite Starter Template' },
   ],
 })
 </script>
+
+<template>
+  <RouterView />
+</template>
